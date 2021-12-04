3e4d3c71e477649af6339314f7d1317ff53fb502
Rteske <ryanteske@outlook.com>
2021-12-04 14:39:02 -0800

    Edit schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_213128) do
+ActiveRecord::Schema.define(version: 2021_12_04_194644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
-    t.integer "number_of_bins"
-    t.string "street_address"
-    t.string "city"
-    t.string "state"
-    t.string "pickup_day"
     t.bigint "driver_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -34,5 +29,19 @@ ActiveRecord::Schema.define(version: 2021_12_03_213128) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "pickup_day"
+    t.integer "number_of_bins"
+    t.boolean "picked_up"
+    t.bigint "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_locations_on_customer_id"
+  end
+
   add_foreign_key "customers", "drivers"
+  add_foreign_key "locations", "customers"
 end
