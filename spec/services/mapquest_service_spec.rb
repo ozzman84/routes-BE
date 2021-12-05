8882f50ca533436b2efe2efd4266c2fd9dfcd190
Kelsey Thompson <kelsthompson2@gmail.com>
2021-12-05 10:06:45 -0700

    Test and write implementation code for route endpoint

---
+++ spec/services/mapquest_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe MapquestService do
+  it 'gets the optimized route for given locations' do
+    locations = ["8242 George Early Rd, Centerville, IN 47330", "706 Poplar Avenue, Centerville, IN 47330", "203 McMinn Rd, Centerville, IN 47330", "9021 College Corner Rd, Greens Fork IN, 47345"]
+    response = MapquestService.get_route(locations)
+
+    expect(response).to be_a(Hash)
+    expect(response).to have_key(:route)
+
+    expect(response[:route]).to have_key(:boundingBox)
+    expect(response[:route]).to have_key(:formattedTime)
+    expect(response[:route]).to have_key(:legs)
+
+    expect(response[:route][:legs].first).to have_key(:formattedTime)
+  end
+end
