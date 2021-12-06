fc56184b62d5e13926d26e942172b1f8cedccc0b
Kelsey Thompson <kelsthompson2@gmail.com>
2021-12-06 11:58:41 -0700

    Create graphql types for route

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,10 +4,13 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :routeRequest, [RouteType], null: false do
+      argument :driver, Integer, required: true
+      argument :pickup_day, String, required: true
+    end
+
+    def routeRequest(driver:, pickup_day:)
+      RouteFacade.create_route(driver, pickup_day)
     end
   end
 end
