1a1de7c912102e23f6f491b206bb97a8df650501
Ozzie <57572826+ozzman84@users.noreply.github.com>
2021-12-11 13:04:30 -0700

    Remove commented out lines & location update command

--- app/graphql/mutations/update_location.rb
+++ app/graphql/mutations/update_location.rb
@@ -8,30 +8,13 @@ module Mutations
     argument :number_of_bins, Integer, required: false
     argument :picked_up, Boolean, required: false
     argument :customer_id, Integer, required: false
-    field :success, Boolean, null: false
-    field :errors, [String], null: false
-    field :location, Types::LocationType, null: true
 
     type Types::LocationType
 
     def resolve(id:, **args)
       location = Location.find(id)
-      # location.update(args)
-
-      if location.update(args)
-        # binding.pry
-        {
-          # success: true,
-          # errors: [],
-          location: location
-        }
-      else
-        {
-          success: false
-          # location: location
-        }
-      end
-      # location
+      location.update(args)
+      location
     end
   end
 end
