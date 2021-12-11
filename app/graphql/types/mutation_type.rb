640a05f437ed7fa55240278927a3a62d55fbbf30
Ozzie <57572826+ozzman84@users.noreply.github.com>
2021-12-11 12:52:41 -0700

    Add update field

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :createCustomer, mutation: Mutations::CreateCustomer
     field :createLocation, mutation: Mutations::CreateLocation
+    field :updateLocation, mutation: Mutations::UpdateLocation
   end
 end
