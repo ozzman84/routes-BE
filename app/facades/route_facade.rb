e5556ece45132bcc7ab0e1c2421dbce8ae5ab47f
Ryan Teske <77249542+Rteske@users.noreply.github.com>
2021-12-11 11:39:28 -0800

    Remove pry

--- app/facades/route_facade.rb
+++ app/facades/route_facade.rb
@@ -2,7 +2,7 @@ class RouteFacade
   class << self
     def create_route(driver, pickup_day)
       stops = Location.route_by_day_and_driver(driver, pickup_day)
-      binding.pry
+
       addresses = full_address(stops, driver)
 
       route_data = MapquestService.get_route(addresses)
