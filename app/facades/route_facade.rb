db14cf602c19464da309e93a26de1a6ae3e8df96
Rteske <ryanteske@outlook.com>
2021-12-13 12:36:02 -0800

    Refactor route facade for use of customer id

--- app/facades/route_facade.rb
+++ app/facades/route_facade.rb
@@ -1,5 +1,31 @@
 class RouteFacade
   class << self
+    def sand_box(args)
+      if args[:driver]
+        stops = Location.route_by_day_and_driver(args[:driver], args[:pickupDay])
+
+        addresses = full_address(stops, args[:driver])
+
+        route_data = MapquestService.get_route(addresses)
+        location_data = route_data[:route][:locations]
+
+        Route.stops(location_data)
+      elsif args[:customer]
+        customer = Customer.find(args[:customer])
+        driver = customer.driver.id
+
+        stops = Location.route_by_day_and_driver(driver, args[:pickupDay])
+        binding.pry
+
+        addresses = full_address(stops, driver)
+
+        route_data = MapquestService.get_route(addresses)
+        location_data = route_data[:route][:locations]
+
+        Route.stops(location_data)
+      end
+    end
+
     def create_route(driver, pickup_day)
       stops = Location.route_by_day_and_driver(driver, pickup_day)
 
